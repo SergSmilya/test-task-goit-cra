113bdbaf102ed91209d49a18ecc098792428d9aa
SergSmilya <sergsmilichenko@gmail.com>
2023-05-09 14:48:50 +0300

    Updates

---
+++ static/js/983.717474f0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktest_task_goit_cra=self.webpackChunktest_task_goit_cra||[]).push([[983],{3983:function(t,e,n){n.r(e),n.d(e,{default:function(){return s}});var r=n(184);function s(){return(0,r.jsx)("div",{children:"HomePage"})}}}]);
+//# sourceMappingURL=983.717474f0.chunk.js.map
